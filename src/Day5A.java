e5a4db977a9e97bca3d28e921fbf6a4336cdf2af
Nate Webber <nwebber@u.rochester.edu>
2022-10-12 16:11:41 -0400

    continue trying 5B

--- src/Day5A.java
+++ src/Day5A.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /*
@@ -9,20 +10,110 @@ import java.util.Scanner;
 
 public class Day5A {
     public static void main(String[] args) throws FileNotFoundException {
-        File inFile = new File("./input/Template.txt");
+        File inFile = new File("./input/Day5A.txt");
         Scanner inScanner = new Scanner(inFile);
-        System.out.println(inScanner.nextLine());
+        int niceCountA = 0;
+        int niceCountB = 0;
+        while (inScanner.hasNextLine()) {
+            String s = inScanner.nextLine();
+            if (stringIsNiceA(s))
+                niceCountA++;
+            if (stringIsNiceB(s))
+                niceCountB++;
+        }
+        System.out.printf("Part A nice strings: %d\n", niceCountA);
+        System.out.printf("Part B nice strings: %d\n", niceCountB);
         inScanner.close();
     }
 
-    private boolean stringIsNice(String s){
+    private static boolean stringIsNiceA(String s) {
         char[] charArr = s.toCharArray();
         int vowelCount = 0;
-        String[] forbiddenStrings = {"ab", "cd", "pq", "xy"};
         boolean foundDouble = false;
-        for (int i = 0; i < charArr.length; i++){
-            
+        for (int i = 0; i < charArr.length; i++) {
+            if (!foundDouble && i >= 1)
+                if (charArr[i] == charArr[i - 1])
+                    foundDouble = true;
+            if (i > 0) {
+                switch (charArr[i]) {
+                    case 'b':
+                        if (charArr[i - 1] == 'a')
+                            return false;
+                        break;
+                    case 'd':
+                        if (charArr[i - 1] == 'c')
+                            return false;
+                        break;
+                    case 'q':
+                        if (charArr[i - 1] == 'p')
+                            return false;
+                        break;
+                    case 'y':
+                        if (charArr[i - 1] == 'x')
+                            return false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            switch (charArr[i]) {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    vowelCount++;
+                    break;
+                default:
+                    break;
+            }
         }
+        return (foundDouble && (vowelCount >= 3));
+    }
 
+    private static boolean stringIsNiceB(String s) {
+        boolean trioFound = false;
+        boolean pairFound = false;
+        System.out.printf("%s\n", s);
+        for (int i = 0; i < (s.length() - 2); i++) {
+            String trio = s.substring(i, i + 3);
+            // System.out.printf("%s\n", trio);
+            if (trio.charAt(0) == trio.charAt(2)) {
+                trioFound = true;
+                break;
+            }
+        }
+        ArrayList<String> pairs = new ArrayList<>();
+        for (int i = 0; i < (s.length() - 1); i++) {
+            String pair = s.substring(i, i + 2);
+            System.out.printf("%d : %s\n", i, pair);
+            if (pairs.contains(pair)) {
+                if (!(pair.charAt(0) == pair.charAt(1))) {
+                    pairFound = true;
+
+                } else {
+                    if (i == 0) {
+                        if (!(pair.charAt(1) == s.charAt(i + 2))) {
+                            pairFound = true;
+
+                        }
+                    } else if (i == (s.length() - 2)) {
+                        if (!(pair.charAt(0) == s.charAt(i - 1))) {
+                            pairFound = true;
+
+                        }
+                    } else {
+                        if (!(pair.charAt(0) == s.charAt(i - 1)) && !(pair.charAt(1) == s.charAt(i +
+                                2))) {
+                            pairFound = true;
+
+                        }
+                    }
+                }
+            }
+            pairs.add(pair);
+        }
+        System.out.printf("trio: %s - pair: %s\n", trioFound, pairFound);
+        return trioFound && pairFound;
     }
 }
